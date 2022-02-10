fbd1a70f7898f9f62cfb2e70d3c3a80581d1fe78
Whoeza <84461456+Whoeza@users.noreply.github.com>
2022-02-10 19:30:23 +0100

    py file

---
+++ wordle-solve.py
@@ -0,0 +1,10 @@
+# UK dictionary from: https://www-cs-faculty.stanford.edu/~knuth/sgb-words.txt
+
+# Step 1.
+# Calculate statistics for each letter of the alphabet A-Z
+
+# Step 2.
+# Assign a score to each word based on how many different letters they have from the subset of most common ones
+
+# Step 3.
+# Print the best word choice for the input dictionary
